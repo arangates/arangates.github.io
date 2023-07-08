f1fee51e9640232cfbf0c03cd9ad930139aeaacf
Aranganathan Rathinavelu <arangates@gmail.com>
2023-07-09 01:46:07 +0200

    Add events

--- pages/events.tsx
+++ pages/events.tsx
@@ -9,8 +9,8 @@ export default function Events({
       title="Pregnancy plan – Aranganathan Rathinavelu"
       description="Planning..."
     >
-      <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
-        <iframe style={{border: '1px solid rgba(0, 0, 0, 0.1)'}} width={800} height={450} src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FmwkNz5akdDvvZrWy0hbIJg%2FPregnancy-Holiday-Plan%3Ftype%3Dwhiteboard%26node-id%3D0%253A1%26t%3DOKGaxKnNgcN6KAZz-1" allowFullScreen />
+      <div className="flex flex-col items-start justify-center  mx-auto mb-16">
+        <iframe style={{border: '1px solid rgba(0, 0, 0, 0.1)'}} width={1500} height={1450} src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FmwkNz5akdDvvZrWy0hbIJg%2FPregnancy-Holiday-Plan%3Ftype%3Dwhiteboard%26node-id%3D0%253A1%26t%3DOKGaxKnNgcN6KAZz-1" allowFullScreen />
       </div>
     </Container>
   );
