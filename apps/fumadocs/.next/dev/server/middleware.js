4a79947576e2ede1b8390ad162cda223348498a2
Aranganathan Rathinavelu <arangates@gmail.com>
2026-09-12 21:58:15 +0200

    Remove obsolete Next.js development files and type definitions to clean up the project structure.

--- apps/fumadocs/.next/dev/server/middleware.js
+++
@@ -1,5 +0,0 @@
-var R=require("./chunks/[turbopack]_runtime.js")("server/middleware.js")
-R.c("server/chunks/node_modules__pnpm_062r2-k._.js")
-R.c("server/chunks/[root-of-the-server]__0y3kkul._.js")
-R.m("[project]/node_modules/.pnpm/next@16.3.4_@babel+core@7.29.7_@types+node@26.5.0_babel-plugin-react-compiler@1.0.0_rea_730c2ded1de5aaba15cc73b3ab4057a8/node_modules/next/dist/esm/build/templates/middleware.js { INNER_MIDDLEWARE_MODULE => \"[project]/apps/fumadocs/proxy.ts [middleware] (ecmascript)\" } [middleware] (ecmascript)")
-module.exports=R.m("[project]/node_modules/.pnpm/next@16.3.4_@babel+core@7.29.7_@types+node@26.5.0_babel-plugin-react-compiler@1.0.0_rea_730c2ded1de5aaba15cc73b3ab4057a8/node_modules/next/dist/esm/build/templates/middleware.js { INNER_MIDDLEWARE_MODULE => \"[project]/apps/fumadocs/proxy.ts [middleware] (ecmascript)\" } [middleware] (ecmascript)").exports
